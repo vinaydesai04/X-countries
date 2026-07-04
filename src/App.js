7a5380ba24eb822f5ea28ea623da3e6b8d90be75
Vinay M Desai <vinaymd2002@gmail.com>
2026-07-04 14:12:55 +0530

    Remove console log from data fetching
    Removed console log for fetched data.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         );
         const data = await response.json();
         setCountries(data);
-        console.log(data);
       } catch (error) {
         console.error("Error fetching data: ", error);
       } finally {
